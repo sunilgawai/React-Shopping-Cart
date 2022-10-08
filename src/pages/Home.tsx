5a39d014fdd48ff393a6ab5122ceb8ead9da1b67
Sunil Gawai <sunilgawai7397@gmail.com>
2022-10-09 01:31:04 +0530

    cart component added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/Header';
 import Products from '../components/Products';
 import { CartContext } from '../context';
 
 const Home = () => {
   return (
     <div>
+      {/* <Header /> */}
       <Products />
     </div>
   )
